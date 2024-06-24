94362e04a71b62b89c573a9f0a7f02751b3a24d5
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-06-24 20:00:11 -0300

    Update dashboard.component.ts

--- dashboard.component.ts
+++ dashboard.component.ts
@@ -1,2 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import { FinancialService } from '../financial.service';
+
+@Component({
+  selector:'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit{
+  expensesData: any;
+  incomeData: any;
+  financialPlan: any;
+
+constructor(private financialService: FinancialService){}
+
+ngOnInit(): void {
+  this.financialService.getExpenses().subscribe
